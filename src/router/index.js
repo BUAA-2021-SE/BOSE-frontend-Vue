d129c7338c4e4f84481caea3b7a6fd78824b72da
LLLeoLi <1097261154@qq.com>
2022-04-30 05:02:06 +0800

    修改路由为哈希模式，添加路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -6,42 +6,71 @@ import Login from '@/components/Login'
 import Register from '@/components/Register'
 import Profile from '@/components/Profile'
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/ping',
-    name:'Ping',
-    component: Ping
-  },
-  {
-    path: '/login',
-    name:'Login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name:'Register',
-    component: Register
-  },
-  {
-    path: '/profile',
-    name:'Profile',
-    component: Profile
-  }
-]
-
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  routes:[
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+      // 添加了路由元信息，只有经过身份验证的用户才能进行操作
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/ping',
+      name:'Ping',
+      component: Ping,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name:'Login',
+      component: Login,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/register',
+      name:'Register',
+      component: Register,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/profile',
+      name:'Profile',
+      component: Profile,
+      meta: {
+        requiresAuth: true
+      }
+    }
+  ]
 })
 // https://router.vuejs.org/zh/guide/advanced/navigation-guards.html
-router.beforeEach(()=>{
-
+// https://router.vuejs.org/zh/guide/advanced/meta.html
+// $route.matched数组记录了路由匹配到的所有路由记录
+// js中的some方法用于检测数组中是否存在元素符合条件，若存在则返回true
+// fullPath = path + query + hash
+router.beforeEach((to,from,next)=>{
+  const token = window.localStorage.getItem('token');
+  if(to.matched.some(record=>record.meta.requiresAuth)&&(!token || token === null)){
+    next({
+      path:'/login',
+      query:{redirect:to.fullPath}
+    });
+  }
+  else if(token && to.name == 'Login'){
+    next({
+      path: from.fullPath
+    });
+  }
+  else{
+    next();
+  }
 })
 export default router
