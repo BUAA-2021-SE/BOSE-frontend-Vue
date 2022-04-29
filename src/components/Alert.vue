b980632b24f304399fccdc754a7397a6590bc184
LLLeoLi <1097261154@qq.com>
2022-04-30 04:39:29 +0800

    测试alert及其对应父组件间传值

---
+++ src/components/Alert.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="alert" role="alert" :class="'alert-' + variant">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name:'alert',
+  props: ['variant', 'message']
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <alert 
+      v-for="(alert, index) in alertsFilter" :key="index"
+      :variant="alert.alertVariant"
+      :message="alert.alertMessage">
+    </alert>
+    <button type="button" class="btn btn-primary">HomePage</button>
+  </div>
+</template>
+
+<script>
+import Alert from './Alert'
+
+export default {
+  name: 'Home',  
+  components: {
+    alert: Alert
+  },
+  data () {
+    return {
+      alerts: [
+        {
+          showAlert: true,
+          alertVariant: 'danger',
+          alertMessage: 0
+        },
+        {
+          showAlert: true,
+          alertVariant: 'info',
+          alertMessage: 1
+        },
+        {
+          showAlert: true,
+          alertVariant: 'dark',
+          alertMessage: 2
+        }
+      ]
+    }
+  },
+  computed:{
+      alertsFilter: function(){
+          return this.alerts.filter((alert)=>{
+              return alert.showAlert;
+          })
+      }
+  }
+}
+</script>
