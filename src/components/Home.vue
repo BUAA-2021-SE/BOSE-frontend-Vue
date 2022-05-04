d02cbc9c0a2225f1d84bf4233da092ca925fe98b
LLLeoLi <1097261154@qq.com>
2022-05-05 02:36:43 +0800

    tomorrow is another day!

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -75,33 +75,9 @@ export default {
       }
   },
   methods:{
-
     getText(){
       console.log(this.value)
     },
-    // drawLine(){
-    //   let myChart = this.$echarts.init(document.getElementById("a"))
-    //   let options = {
-    //     xAxis: {
-    //       type: 'category',
-    //       data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    //     },
-    //     yAxis: {
-    //       type: 'value'
-    //     },
-    //     series: [
-    //       {
-    //         data: [150, 230, 224, 218, 135, 147, 260],
-    //         type: 'line'
-    //       }
-    //     ]
-    //   };
-    //   myChart.setOption(options);
-    // },
-   
-  },
-  mounted(){
-    this.drawLine();
   },
   
 }
