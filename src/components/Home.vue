1fdda5af754d309e5be9c02a570ce1962683a9b4
echo17666 <72566693+echo17666@users.noreply.github.com>
2022-05-06 01:55:15 +0800

    feat: change mavon-editor toolbars

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,7 +14,7 @@
       <input type="text" v-model="postForm.summary" class="form-control" id="post_summary" placeholder="摘要">
     </div>
     <div class="form-group">
-      <mavon-editor v-model="postForm.body"/>
+      <mavon-editor v-model="postForm.body" :toolbars="tools" />
       <small class="form-control-feedback" v-show="postForm.bodyError">{{ postForm.bodyError }}</small>
     </div>
     <button  class="btn btn-primary" @click="aaa">Submit</button>
@@ -80,7 +80,34 @@ export default {
         errors: 0,  // 表单是否在前端验证通过，0 表示没有错误，验证通过
         titleError: false,
         bodyError: false
-      }
+      },
+    tools: {
+    bold: true, // 粗体
+    italic: true, // 斜体
+    header: true, // 标题
+    underline: true, // 下划线
+    strikethrough: true, // 中划线
+    mark: true, // 标记
+    superscript: true, // 上角标
+    subscript: true, // 下角标
+    quote: true, // 引用
+    ol: true, // 有序列表
+    ul: true, // 无序列表
+    link: true, // 链接
+    imagelink: true, // 图片链接
+    code: true, // code
+    table: true, // 表格
+    fullscreen: true, // 全屏编辑
+    readmodel: true, // 沉浸式阅读
+    htmlcode: true, // 展示html源码
+    help: true, // 帮助
+    undo: true, // 上一步
+    redo: true, // 下一步
+    trash: true, // 清空
+    navigation: true, // 导航目录
+    subfield: true, // 单双栏模式
+    preview: true // 预览
+  },
     }
   },
   computed:{
