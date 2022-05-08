9d8101dd18a1ba07e54b473f13467723f67a5416
LLLeoLi <1097261154@qq.com>
2022-05-09 00:07:45 +0800

    把编辑博文单独作为一个界面

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,8 @@ import Reset from '@/components/account/Reset'
 import Profile from '@/components/account/Profile'
 import Ping from '@/components/Ping'
 import EditProfile from '@/components/account/EditProfile'
-import Post from '@/components/Post'
+import Post from '@/components/Blog/Blog'
+import PostEdit from '@/components/Blog/EditBlog'
 import Followers from '@/components/profile/Followers'
 import Following from '@/components/profile/Following'
 import Posts from '@/components/profile/Posts'
@@ -25,7 +26,11 @@ const router = new VueRouter({
     {
       path: '/post/:id',
       name: 'Post',
-      component: Post
+      component: Post,
+    },
+    { path: '/postedit/:id',
+      name: 'PostEdit',
+      component: PostEdit,
     },
     {
       path: '/ping',
