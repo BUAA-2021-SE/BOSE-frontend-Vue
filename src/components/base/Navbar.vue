79697d0c5cd91faa99232d0073180c7eb9a374d9
LLLeoLi <1097261154@qq.com>
2022-05-15 00:44:03 +0800

    一些路由跳转修改

--- src/components/base/Navbar.vue
+++ src/components/base/Navbar.vue
@@ -52,7 +52,7 @@
 
     <router-link
       v-if="sharedState.is_authenticated"
-      :to="{ name: 'PostEdit', params: { id: sharedState.user_id } }"
+      :to="{ name: 'PostAdd', params: { id: sharedState.user_id } }"
     >
       <v-btn text class="white--text"> New Blog </v-btn>
     </router-link>
@@ -143,90 +143,7 @@ export default {
                 ],
               },
             },
-            // {
-            //   type: "text",
-            //   left: "center",
-            //   top: "center",
-            //   style: {
-            //     text: " O  ",
-            //     fontSize: 40,
-            //     fontWeight: "bold",
-            //     lineDash: [0, 200],
-            //     lineDashOffset: 0,
-            //     fill: "transparent",
-            //     stroke: "rgba(0,0,0,0.3)",
-            //     lineWidth: 1,
-            //   },
-            //   keyframeAnimation: {
-            //     duration: 4000,
-            //     //loop: true,
-            //     keyframes: [
-            //       {
-            //         percent: 0.7,
-            //         style: {
-            //           fill: "transparent",
-            //           lineDashOffset: 200,
-            //           lineDash: [200, 0],
-            //         },
-            //       },
-            //       {
-            //         // Stop for a while.
-            //         percent: 0.8,
-            //         style: {
-            //           fill: "transparent",
-            //         },
-            //       },
-            //       {
-            //         percent: 1,
-            //         style: {
-            //           fill: "rgba(0,0,200,0.75)",
-            //         },
-            //       },
-            //     ],
-            //   },
-            // },
-            // {
-            //   type: "text",
-            //   left: "center",
-            //   top: "center",
-            //   style: {
-            //     text: "  SE",
-            //     fontSize: 40,
-            //     fontWeight: "bold",
-            //     lineDash: [0, 200],
-            //     lineDashOffset: 0,
-            //     fill: "transparent",
-            //     stroke: "rgba(0,0,0,0.5)",
-            //     lineWidth: 1,
-            //   },
-            //   keyframeAnimation: {
-            //     duration: 5000,
-            //     //loop: true,
-            //     keyframes: [
-            //       {
-            //         percent: 0.7,
-            //         style: {
-            //           fill: "transparent",
-            //           lineDashOffset: 200,
-            //           lineDash: [200, 0],
-            //         },
-            //       },
-            //       {
-            //         // Stop for a while.
-            //         percent: 0.8,
-            //         style: {
-            //           fill: "transparent",
-            //         },
-            //       },
-            //       {
-            //         percent: 1,
-            //         style: {
-            //           fill: "rgba(0,100,50,0.75)",
-            //         },
-            //       },
-            //     ],
-            //   },
-            // },
+          
           ],
         },
       };
