efe98aa3b896417184e1e324b0deae2225e18c2c
LLLeoLi <1097261154@qq.com>
2022-05-16 02:28:50 +0800

    blog组件化重构

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,56 +7,9 @@
         :message="alert.alertMessage">
     </alert>
     <div>
-      <v-card
-          class="mx-auto"
-          v-for="(post, index) in posts"
-          :key="index"
-      >
-        <router-link
-            :to="{name:'Post',params:{id:post.id} }"
-        >
-          <v-card-title>
-            {{ post.title }}
-          </v-card-title>
-        </router-link>
-        <v-card-text>
-          <p>{{ post.author.name || post.author.username }}</p>
-          <!-- <p>echo17666</p> -->
-          <div class="text--primary">
-            {{ post.summary }}
-          </div>
-        </v-card-text>
-        <v-card-actions>
-          <router-link
-              :to="{name:'Post',params:{id:post.id} }"
-          >
-            <v-btn
-                text
-                color="deep-purple accent-4"
-            >
-              阅读全文
-            </v-btn>
-          </router-link>
-          <v-btn
-              v-if="post.author.id==sharedState.user_id"
-              text
-              color="deep-purple accent-4"
-              @click="showDeleteDialog(post.id)"
-          >
-            删除
-          </v-btn>
-          <router-link
-              :to="{name:'PostEdit',params:{id:post.id} }">
-            <v-btn
-                v-if="post.author.id==sharedState.user_id"
-                text
-                color="deep-purple accent-4"
-            >
-              编辑
-            </v-btn>
-          </router-link>
-        </v-card-actions>
-      </v-card>
+      <blog v-for="(post,index) in posts" :key="index"
+      :post = "post">
+      </blog>
       <span>共有博文{{ total }}篇</span>
       <v-pagination
           v-model="page"
@@ -64,35 +17,6 @@
           :total-visible="7"
           circle
       ></v-pagination>
-      <!-- 删除弹出框 -->
-      <v-dialog style="z-index:2000"
-                v-model="showDelete"
-                width="25vw"
-                height="20vh">
-        <v-card :style="{width:'25vw', height:'12vh'}" class="d-flex align-content-end flex-wrap">
-          <v-card-title class="mx-auto">
-            确定删除？
-          </v-card-title>
-          <v-card-actions>
-            <v-btn
-                color="primary"
-                text
-                @click="showDelete= false"
-            >
-              退出
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn
-                color="error"
-                text
-                @click="onDeletePost"
-            >
-              确认
-            </v-btn>
-          </v-card-actions>
-
-        </v-card>
-      </v-dialog>
     </div>
   </div>
 </template>
@@ -103,12 +27,13 @@ import Alert from './base/Alert.vue'
 import store from '../store.js'
 import VueMarkdown from 'vue-markdown'
 import Post from '@/api/post'
-
+import BlogItem from '@/components/base/BlogItem.vue'
 export default {
   name: 'Home',
   components: {
     alert: Alert,
-    VueMarkdown
+    VueMarkdown,
+    blog: BlogItem
   },
   data() {
     return {
@@ -136,8 +61,6 @@ export default {
         },
       ],
       posts: '',
-      showDelete: false,
-      deleteId: 0,
       tools: {
         bold: true, // 粗体
         italic: true, // 斜体
@@ -193,38 +116,6 @@ export default {
             this.page = res.data.page
             this.size = res.data.size
             this.pageTotal = Math.ceil(this.total / this.size)
-          })
-    },
-    showDeleteDialog(id) {
-      this.deleteId = id;
-      this.showDelete = true;
-    },
-    onEditPost() {
-      console.log("onEditPost");
-    },
-    onSubmitUpdate() {
-      console.log("onSubmitUpdate");
-    },
-    onResetUpdate() {
-      console.log("onResetUpdate");
-    },
-    onDeletePost() {
-      console.log("onDelete", this.deleteId);
-      Post.deleteBlog(this.deleteId)
-          .then((res) => {
-            console.log(res);
-            this.deleteId = 0;
-            this.showDelete = false;
-            this.$toasted.success(res.data,
-                {
-                  icon: 'check',
-                  fullWidth: true,
-                  position: "bottom-center"
-                })
-            this.getPosts(1)
-          })
-          .catch((err) => {
-            console.error(err, "not deleted");
           })
     }
   },
---
+++ src/components/base/BlogItem.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+  <v-card class="mx-auto">
+        <router-link
+            :to="{name:'Post',params:{id:post.id} }"
+        >
+          <v-card-title>
+            {{ post.title }}
+          </v-card-title>
+        </router-link>
+        <v-card-text>
+          <p>{{ post.author.name || post.author.username }}</p>
+          <div class="text--primary">
+            {{ post.summary }}
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <router-link
+              :to="{name:'Post',params:{id:post.id} }"
+          >
+            <v-btn
+                text
+                color="deep-purple accent-4"
+            >
+              阅读全文
+            </v-btn>
+          </router-link>
+          <v-btn
+              v-if="post.author.id==sharedState.user_id"
+              text
+              color="deep-purple accent-4"
+              @click="showDeleteDialog(post.id)"
+          >
+            删除
+          </v-btn>
+          <router-link
+              :to="{name:'PostEdit',params:{id:post.id} }">
+            <v-btn
+                v-if="post.author.id==sharedState.user_id"
+                text
+                color="deep-purple accent-4"
+            >
+              编辑
+            </v-btn>
+          </router-link>
+        </v-card-actions>
+      </v-card>
+      <v-dialog style="z-index:2000"
+                v-model="showDelete"
+                width="25vw"
+                height="20vh">
+        <v-card :style="{width:'25vw', height:'12vh'}" class="d-flex align-content-end flex-wrap">
+          <v-card-title class="mx-auto">
+            确定删除？
+          </v-card-title>
+          <v-card-actions>
+            <v-btn
+                color="primary"
+                text
+                @click="showDelete= false"
+            >
+              退出
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn
+                color="error"
+                text
+                @click="onDeletePost"
+            >
+              确认
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+  </div>
+</template>
+
+<script>
+import store from '@/store.js'
+export default {
+    props:['post'],
+    data(){
+        return {
+            sharedState: store.state,
+            showDelete: false,
+            deleteId: 0,
+        }
+    },
+    methods:{
+      showDeleteDialog(id) {
+        this.deleteId = id;
+        this.showDelete = true;
+      },
+      onDeletePost() {
+        console.log("onDelete", this.deleteId);
+        Post.deleteBlog(this.deleteId)
+            .then((res) => {
+              console.log(res);
+              this.deleteId = 0;
+              this.showDelete = false;
+              this.$toasted.success(res.data,
+                  {
+                    icon: 'check',
+                    fullWidth: true,
+                    position: "bottom-center"
+                  })
+              this.getPosts(1)
+            })
+            .catch((err) => {
+              console.error(err, "not deleted");
+            })
+      }
+    }
+}
+</script>
