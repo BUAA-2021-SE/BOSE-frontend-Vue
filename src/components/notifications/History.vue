95a249cf70febf777c826a3b7c4ef017df3e4b59
LLLeoLi <1097261154@qq.com>
2022-05-28 19:54:40 +0800

    历史记录

--- src/components/notifications/History.vue
+++ src/components/notifications/History.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
   历史记录
+  {{resdata}}
   </div>
 </template>
 
 <script>
+import Notifications from '@/api/notifications';
 export default {
-    name:'History'
+  name:'History',
+  data(){
+    return{
+      resdata:[]
+    }
+  },
+  methods: {
+    getHistoryList(){
+      console.log('getHistoryList');
+      Notifications.getHistoryList()
+      .then((res)=>{
+        console.log(res);
+        this.resdata = res;
+      })
+      .catch((err) => {
+        console.log(err.response.details);
+      })
+    }
+  },
+  created() {
+    this.getHistoryList();
+  },
+  beforeRouteUpdate(to, from, next) {
+    next();
+    this.getHistoryList();
+  }
 }
 </script>
 
