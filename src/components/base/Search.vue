883f8687baa2b84a14c0c75449cf8b74287ada6a
hjc-owo <2535199657@qq.com>
2022-06-01 16:55:52 +0800

    来点搜索高亮显示初稿2，整不出来诶

--- src/components/base/Search.vue
+++ src/components/base/Search.vue
@@ -22,11 +22,87 @@
         <div class="text-align-center">
           <h6 class="text-align-center">共有博文{{ total }}篇</h6>
         </div>
-        <v-col cols="12" sm="6" md="6" v-for="(post,index) in posts" :key="index">
-          <blog
-              :post="brightSearch(post)"
-              @delete="getSearchPosts(1)">
-          </blog>
+        <v-col cols="12" sm="6" md="6">
+          <div v-for="(post,index) in posts" :key="index">
+            <div>
+              <v-card outlined class="mx-auto">
+                <v-row>
+                  <v-col cols="12" md="7">
+                    <router-link :to="{ name: 'Post', params: { id: post.id } }">
+                      <v-card-title>
+                        <h3 v-if="post.title.length <12">{{ post.title }}</h3>
+                        <h3 v-else>{{ post.title.substring(0, 9) + '...' }}</h3>
+                      </v-card-title>
+                    </router-link>
+                    <v-card-text>
+                      <p>{{ post.author.name || post.author.username }}</p>
+                      <div class="text--primary" v-if="post.summary.length <30">
+                        {{ post.summary }}
+                      </div>
+                      <div class="text--primary" v-else>
+                        {{ post.summary.substring(0, 27) + '...' }}
+                      </div>
+                    </v-card-text>
+                    <v-card-actions>
+                      <router-link :to="{ name: 'Post', params: { id: post.id } }">
+                        <v-btn text color="deep-purple accent-4"> 阅读全文</v-btn>
+                      </router-link>
+                      <v-btn
+                          v-if="post.author.id == sharedState.user_id"
+                          text
+                          color="deep-purple accent-4"
+                          @click="showDeleteDialog(post.id)"
+                      >
+                        删除
+                      </v-btn>
+                      <router-link :to="{ name: 'PostEdit', params: { id: post.id } }">
+                        <v-btn
+                            v-if="post.author.id == sharedState.user_id"
+                            text
+                            color="deep-purple accent-4"
+                        >
+                          编辑
+                        </v-btn>
+                      </router-link>
+                    </v-card-actions>
+                  </v-col>
+                  <v-col cols="12" md="2"></v-col>
+                  <v-col class="d-flex justify-end" cols="12" md="3">
+                    <v-img
+                        :src="post.cover"
+                        class="my-auto"
+                        contain
+                        height="150"
+                        max-width="200"
+                        max-height="150"
+                        :style="{'margin-right':'10px'}"
+                    />
+                  </v-col>
+                </v-row>
+
+              </v-card>
+              <v-dialog
+                  style="z-index: 2000"
+                  v-model="showDelete"
+                  width="25vw"
+                  height="20vh"
+              >
+                <v-card
+                    :style="{ width: '25vw', height: '20vh' }"
+                    class="d-flex align-center flex-wrap"
+                >
+                  <v-row class="mx-auto d-flex justify-center">
+                    <v-card-title class="mx-auto"><h3 class="mx-auto">确定删除？</h3></v-card-title>
+                    <v-card-actions>
+                      <v-btn color="primary" text @click="showDelete = false"> 取消</v-btn>
+                      <v-spacer></v-spacer>
+                      <v-btn color="error" text @click="onDeletePost"> 确认</v-btn>
+                    </v-card-actions>
+                  </v-row>
+                </v-card>
+              </v-dialog>
+            </div>
+          </div>
         </v-col>
       </v-row>
       <div>
@@ -50,17 +126,18 @@
 
 <script>
 import Post from "@/api/post";
-import BlogItem from '@/components/base/BlogItem.vue'
+import store from "@/store";
 
 export default {
-  name: 'Posts',
-  components: {
-    blog: BlogItem
-  },
+  name: 'Search',
   data() {
     return {
+      keywords: this.$route.params.keyword,
       posts: [],
       loadingProfile: true,
+      sharedState: store.state,
+      showDelete: false,
+      deleteId: 0,
       blog: {
         title: "",
         summary: "",
@@ -106,6 +183,28 @@ export default {
         return post
       }
     },
+    showDeleteDialog(id) {
+      this.deleteId = id;
+      this.showDelete = true;
+    },
+    onDeletePost() {
+      console.log("onDelete", this.deleteId);
+      Post.deleteBlog(this.deleteId)
+          .then((res) => {
+            console.log(res);
+            this.$emit("delete");
+            this.deleteId = 0;
+            this.showDelete = false;
+            this.$toasted.success(res.data, {
+              icon: "check",
+              fullWidth: true,
+              position: "bottom-center",
+            });
+          })
+          .catch((err) => {
+            console.error(err, "not deleted");
+          });
+    },
   },
   watch: {
     page: function (newPage, oldPage) {
@@ -119,5 +218,28 @@ export default {
     next()
     this.getSearchPosts(1)
   },
-}
+};
 </script>
+<style scoped>
+h3 {
+  margin-top: 0px !important;
+  padding-top: 0px !important;
+}
+
+.router-link-active {
+  text-decoration: none;
+}
+
+a {
+  text-decoration: none;
+  color: rgb(255, 255, 255);
+}
+
+.v-card__title {
+  color: #df2d2d;
+}
+
+.search_text {
+  color: #df2d2d;
+}
+</style>
