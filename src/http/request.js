80b3c2190e49fe99e0f898e711a955adbc3e9555
LLLeoLi <1097261154@qq.com>
2022-06-05 16:45:00 +0800

    token的响应拦截

--- src/http/request.js
+++ src/http/request.js
@@ -1,11 +1,12 @@
+import Vue from 'vue'
 import axios from 'axios'
 import store from '../store.js'
-
+import router from '../router'
 const service = axios.create({
     baseURL: 'http://43.138.58.36:8000/',//服务器
-    // baseURL: 'http://localhost:8000/',//本地
     timeout: 10000 //超时时间
 });
+// 请求拦截器
 service.interceptors.request.use(function (config) {
     // Do something before request is sent
     const token = window.localStorage.getItem('token')
@@ -13,27 +14,35 @@ service.interceptors.request.use(function (config) {
         config.headers.Authorization = `Bearer ${token}`
     }
     return config
+}, function(error){
+    return Promise.reject(error)
+})
+// 响应拦截器
+service.interceptors.response.use(function(response) {
+    return response
 }, function (error) {
     // Do something with request error
-    switch (error.response.status) {
-        // 401 UNAUTHORIZED token过期,要求用户重新登录
-        case 401:
-            // 清除 Token 及 已认证 等状态
-            store.logoutAction()
-            // 跳转到登录页
-            if (router.currentRoute.path !== '/login') {
-                Vue.toasted.error('401: 认证已失效，请先登录', {icon: 'fingerprint'})
-                router.replace({
-                    path: '/login',
-                    query: {redirect: router.currentRoute.path},
-                })
-            }
-            break
-
-        case 404:
-            Vue.toasted.error('404: NOT FOUND', {icon: 'fingerprint'})
-            router.back()
-            break
+    if(error.response){
+        switch (error.response.status) {
+            // 450 UNAUTHORIZED token过期,要求用户重新登录
+            case 450:
+                // 清除 Token 及 已认证 等状态
+                store.logoutAction();
+                // 跳转到登录页
+                if (router.currentRoute.path !== '/login') {
+                    Vue.toasted.error('450: 认证已失效，请先登录', {icon: 'check'})
+                    router.replace({
+                        path: '/login',
+                        query: {redirect: router.currentRoute.path},
+                    })
+                }
+                break
+            // 没有对应资源
+            case 404:
+                Vue.toasted.error('404: NOT FOUND', {icon: 'fingerprint'})
+                router.back()
+                break
+        }
     }
     return Promise.reject(error)
 })
