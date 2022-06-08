d6be8f084ab6ac7112c47c3189a441dfe5f38ef0
hjc-owo <2535199657@qq.com>
2022-06-08 13:42:18 +0800

    搜索页面

--- src/components/search/Index.vue
+++ src/components/search/Index.vue
@@ -4,6 +4,7 @@
       <v-tab :to="{name:'Search'}">综合</v-tab>
       <v-tab :to="{name:'SearchBlog'}">博文</v-tab>
       <v-tab :to="{name:'SearchResource'}">资源</v-tab>
+      <v-tab :to="{name:'SearchTag'}">标签</v-tab>
     </v-tabs>
     <router-view/>
   </div>
