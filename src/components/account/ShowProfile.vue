7daafda11de4c469f728899b6801f6467421a963
echo17666 <72566693+echo17666@users.noreply.github.com>
2022-06-08 15:43:53 +0800

    showprofile界面bootstrap清除

--- src/components/account/ShowProfile.vue
+++ src/components/account/ShowProfile.vue
@@ -15,92 +15,76 @@
       </div>
       <v-card shaped elevation='12'>
         <div
-          class="g-brd-around g-brd-gray-light-v4 g-pa-20 g-mb-40"
+        
           v-show="!loadingProfile"
         >
           <!-- v-divider vertical useless -->
           <div class="col-sm-9">
             <!-- Username -->
             <v-card-title>
-              <h2 v-if="user.name" class="g-font-weight-300 g-mr-10">
+              <h2 v-if="user.name">
                 {{ user.name }}
               </h2>
-              <h2 v-else class="g-font-weight-300 g-mr-10">
+              <h2 v-else>
                 {{ user.username }}
               </h2>
             </v-card-title>
           </div>
         <v-card-subtitle>
           <div
-            class="d-flex align-items-center justify-content-sm-between g-mb-5"
+           
           >
-            <h4
-              class="h6 g-font-weight-300 g-mb-10"
+            <h6
               v-show="!this.loadingProfile"
             >
-              <i
-                class="icon-check g-pos-rel g-top-1 g-color-gray-dark-v5 g-mr-5"
-                v-show="!this.loadingProfile"
-              ></i>
+              <v-icon>verified
+              </v-icon>
               认证用户
-            </h4>
+            </h6>
           </div>
           <!-- End Username -->
 
-          <!-- Member since -->
-          <h4 v-if="user.member_since" class="h6 g-font-weight-300 g-mb-10">
-            <i
-              class="icon-badge g-pos-rel g-top-1 g-color-gray-dark-v5 g-mr-5"
-            ></i>
-            Member since : {{ $moment(user.member_since).format("LLL") }}
-          </h4>
-          <!-- End Member since -->
+         
 
           <!-- Last seen -->
-          <h4 v-if="user.last_seen" class="h6 g-font-weight-300 g-mb-10">
-            <i
-              class="icon-eye g-pos-rel g-top-1 g-color-gray-dark-v5 g-mr-5"
-            ></i>
+          <h6 v-if="user.last_seen" >
+            <v-icon>visibility</v-icon>
             上次查看 : {{ $moment(user.last_seen).fromNow() }}
-          </h4>
+          </h6>
           <!-- End Last seen -->
 
           <!-- User Info -->
-          <h4
+          <a
+            
+              :href="'mailto:' + user.email"
+              >
+          <h6
             v-if="user.email"
-            class="h6 g-font-weight-300 g-mb-10"
+           
             v-show="!this.loadingProfile"
           >
-            <i
-              class="icon-link g-pos-rel g-top-1 g-color-gray-dark-v5 g-mr-5"
-            ></i>
-            <a
-              class="g-color-main g-color-primary--hover"
-              :href="'mailto:' + user.email"
-              >{{ user.email }}</a
+           
+           <v-icon>attach_email</v-icon>
+            {{ user.email }}
+          </h6>
+          </a
             >
-          </h4>
           <!-- End User Info -->
 
           <!-- Location -->
-          <h4 v-if="user.location" class="h6 g-font-weight-300 g-mb-10">
-            <i
-              class="icon-location-pin g-pos-rel g-top-1 g-color-gray-dark-v5 g-mr-5"
-            ></i>
+          <h6 v-if="user.location" >
+            <v-icon>place</v-icon>
             {{ user.location }}
-          </h4>
+          </h6>
           <!-- End Location -->
         </v-card-subtitle>
           <v-card-text v-if="user.about_me">
             <div
-              class="u-divider u-divider-db-dashed u-divider-center g-brd-gray-light-v2 g-mt-50 g-mb-20"
+            
             >
-              <i
-                class="u-divider__icon u-divider__icon--indented g-bg-gray-light-v4 g-color-gray-light-v1 rounded-circle"
-                >我</i
-              >
+             
             </div>
-            <p class="lead g-line-height-1_8">{{ user.about_me }}</p>
+            <p >{{ user.about_me }}</p>
           </v-card-text>
         </div>
       </v-card>
@@ -141,6 +125,7 @@ export default {
         .then((res) => {
           console.log(res.data);
           this.user.name = res.data.name;
+          this.user.email = res.data.email;
           this.user.about_me = res.data.about_me;
           this.user._links.avatar = res.data.headshot;
           this.user.last_seen = res.data.last_seen;
